5be61faf31910fbce71ae86963327253ffb1932f
camilo <camiloalvarez2004@gmail.com>
2023-01-09 16:48:32 -0500

    agrego archivo php

---
+++ sincronizacionLocalGitHub.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	echo "hola, este documento fue creado para comprobar la capacidad de sincronicidad entre los archivos creados en local, administrados con git Bash y la plataforma Github"
+	?>
+</body>
+</html>
